e9a453a5cf6121260fbc667403cedda4e2d0c437
Akram Hany <akramhany65@gmail.com>
2022-11-16 19:53:51 +0200

    Remove "Output::" from the declaration of DrawCircle function and Add a comment

--- GUI/Output.h
+++ GUI/Output.h
@@ -20,7 +20,7 @@ public:
 	
 	// -- Figures Drawing functions
 	void DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected=false) const;  //Draw a rectangle
-	void Output::DrawCircle(Point P1, Point P2, GfxInfo CircleGfxInfo, bool selected) const;
+	void DrawCircle(Point P1, Point P2, GfxInfo CircleGfxInfo, bool selected) const; //Draw a Circle
 	///Make similar functions for drawing all other figure types.
 	
 	void PrintMessage(string msg) const;	//Print a message on Status bar
