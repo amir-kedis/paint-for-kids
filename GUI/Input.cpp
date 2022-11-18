783c6faad01a0af92ec736b96f583d09f8d64467
ahmedhamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2022-11-18 02:29:15 +0200

    Get User Action for Shapes Mode

--- GUI/Input.cpp
+++ GUI/Input.cpp
@@ -80,6 +80,37 @@ ActionType Input::GetUserAction() const
 		//[3] User clicks on the status bar
 		return STATUS;
 	}
+	else if (UI.InterfaceMode == MODE_SHAPES) //GUI in the Shapes mode
+	{
+		//[1] If user clicks on the Toolbar
+		if (y >= 0 && y < UI.ToolBarHeight)
+		{
+			//Check whick Menu item was clicked
+			//==> This assumes that menu items are lined up horizontally <==
+			int ClickedItemOrder = (x / UI.MenuItemWidth);
+			//Divide x coord of the point clicked by the menu item width (int division)
+			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+
+			switch (ClickedItemOrder)
+			{
+			case ITM_RECT:	 return DRAW_RECT;
+			case ITM_SQUARE: return DRAW_SQUARE;
+			case ITM_CIRCLE: return DRAW_CIRCLE;
+			case ITM_TRI:    return DRAW_TRI;
+			case ITM_HEX:    return DRAW_HEX;
+			default: return EMPTY;	//A click on empty place in desgin toolbar
+			}
+		}
+
+		//[2] User clicks on the drawing area
+		if (y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
+		{
+			return DRAWING_AREA;
+		}
+
+		//[3] User clicks on the status bar
+		return STATUS;
+	}
 	else	//GUI is in PLAY mode
 	{
 		///TODO:
