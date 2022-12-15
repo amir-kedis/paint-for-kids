284e3435a022ff34445d5d2ff38b33a8d6077490
Akram Hany <akramhany65@gmail.com>
2022-12-15 17:07:30 +0200

    Finish Implementing the CHexagon class

--- Figures/CHexagon.cpp
+++ Figures/CHexagon.cpp
@@ -1,1 +1,13 @@
 #include "CHexagon.h"
+
+CHexagon::CHexagon(Point P, GfxInfo FigureGfxInfo):CFigure(FigureGfxInfo)
+{
+	Center = P;
+}
+
+
+void CHexagon::Draw(Output* pOut) const
+{
+	//Call Output::DrawHexagon to draw a hexagon on the screen	
+	pOut->DrawHexagon(Center, FigGfxInfo, Selected);
+}
