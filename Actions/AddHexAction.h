f5bfd732b2cf5b222fda4fdea3090f71f46008db
Akram Hany <akramhany65@gmail.com>
2022-12-15 17:24:20 +0200

    Finish writing data members and methods of the AddHexAction.h file

--- Actions/AddHexAction.h
+++ Actions/AddHexAction.h
@@ -3,8 +3,19 @@
 
 #include "Action.h"
 
-class AddHexAction
+class AddHexAction : public Action
 {
+private:
+	Point P1; //Center of Hex
+	GfxInfo HexGfxInfo;
+public:
+	AddHexAction(ApplicationManager* pApp);
+
+	//Reads rectangle parameters
+	virtual void ReadActionParameters();
+
+	//Add rectangle to the ApplicationManager
+	virtual void Execute();
 };
 
 #endif
