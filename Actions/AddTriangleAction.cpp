5c7054cfd41c0021994bb6fd33d3524595ae485a
Akram Hany <akramhany65@gmail.com>
2022-12-15 20:33:04 +0200

    Create AddTriangleAction class (.h and .cpp)

---
+++ Actions/AddTriangleAction.cpp
@@ -0,0 +1,1 @@
+#include "AddTriangleAction.h"
---
+++ Actions/AddTriangleAction.h
@@ -0,0 +1,10 @@
+#ifndef ADD_TRI_ACTION_H
+#define ADD_TRI_ACTION_H
+
+#include "Action.h"
+
+class AddTriangleAction : public Action
+{
+};
+
+#endif
