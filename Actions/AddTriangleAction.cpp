a501d583728c99ed93ef705256c8e9f9d23d3944
Akram Hany <akramhany65@gmail.com>
2022-12-15 20:45:53 +0200

    Finish Implementing AddTriangleAction methods in .cpp file

--- Actions/AddTriangleAction.cpp
+++ Actions/AddTriangleAction.cpp
@@ -1,1 +1,52 @@
 #include "AddTriangleAction.h"
+#include "..\Figures\CTriangle.h"
+
+#include "..\ApplicationManager.h"
+
+#include "..\GUI\input.h"
+#include "..\GUI\Output.h"
+
+AddTriangleAction::AddTriangleAction(ApplicationManager* pApp):Action(pApp)
+{}
+
+void AddTriangleAction::ReadActionParameters()
+{
+	//Get a Pointer to the Input / Output Interfaces
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+
+	pOut->PrintMessage("New Triangle: Click at first corner");
+
+	//Read 1st corner and store in point P1
+	pIn->GetPointClicked(P1.x, P1.y);
+
+	pOut->PrintMessage("New Triangle: Click at second corner");
+
+	//Read 2nd corner and store in point P2
+	pIn->GetPointClicked(P2.x, P2.y);
+
+	pOut->PrintMessage("New Triangle: Click at third corner");
+
+	//Read 3nd corner and store in point P3
+	pIn->GetPointClicked(P3.x, P3.y);
+
+	TriangleGfxInfo.isFilled = false;	//default is not filled
+	//get drawing, filling colors and pen width from the interface
+	TriangleGfxInfo.DrawClr = pOut->getCrntDrawColor();
+	TriangleGfxInfo.FillClr = pOut->getCrntFillColor();
+
+	pOut->ClearStatusBar();
+
+}
+
+void AddTriangleAction::Execute()
+{
+	//This action needs to read some parameters first
+	ReadActionParameters();
+
+	//Create a triangle with the parameters read from the user
+	CTriangle* R = new CTriangle(P1, P2, P3, TriangleGfxInfo);
+
+	//Add the triangle to the list of figures
+	pManager->AddFigure(R);
+}
