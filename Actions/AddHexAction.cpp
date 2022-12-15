e50792c305562f6855e6e1f631b08014a1ef40cc
Akram Hany <akramhany65@gmail.com>
2022-12-15 17:28:36 +0200

    Finish implementin the methods in AddHexAction.cpp file

--- Actions/AddHexAction.cpp
+++ Actions/AddHexAction.cpp
@@ -1,7 +1,43 @@
 #include "AddHexAction.h"
-#include "..\Figures\CRectangle.h"
+#include "..\Figures\CHexagon.h"
 
 #include "..\ApplicationManager.h"
 
 #include "..\GUI\input.h"
 #include "..\GUI\Output.h"
+
+AddHexAction::AddHexAction(ApplicationManager* pApp) :Action(pApp)
+{}
+
+void AddHexAction::ReadActionParameters()
+{
+	//Get a Pointer to the Input / Output Interfaces
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+
+	pOut->PrintMessage("New Hexagon: Click at the center of the hexagon");
+
+	//Read the center and store it in point P1
+	pIn->GetPointClicked(P1.x, P1.y);
+
+
+	HexGfxInfo.isFilled = false;	//default is not filled
+	//get drawing, filling colors and pen width from the interface
+	HexGfxInfo.DrawClr = pOut->getCrntDrawColor();
+	HexGfxInfo.FillClr = pOut->getCrntFillColor();
+
+	pOut->ClearStatusBar();
+
+}
+
+void AddHexAction::Execute()
+{
+	//This action needs to read some parameters first
+	ReadActionParameters();
+
+	//Create a hexagon with the parameters read from the user
+	CHexagon* R = new CHexagon(P1, HexGfxInfo);
+
+	//Add the hexagon to the list of figures
+	pManager->AddFigure(R);
+}
