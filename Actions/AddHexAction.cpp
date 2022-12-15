8195308bcf2b0277394e17341dbab69214746cab
Akram Hany <akramhany65@gmail.com>
2022-12-15 17:21:56 +0200

    Create the AddHexAction.h and AddHexAction.cpp files

---
+++ Actions/AddHexAction.cpp
@@ -0,0 +1,7 @@
+#include "AddHexAction.h"
+#include "..\Figures\CRectangle.h"
+
+#include "..\ApplicationManager.h"
+
+#include "..\GUI\input.h"
+#include "..\GUI\Output.h"
---
+++ Actions/AddHexAction.h
@@ -0,0 +1,10 @@
+#ifndef ADD_HEX_ACTION_H
+#define ADD_HEX_ACTION_H
+
+#include "Action.h"
+
+class AddHexAction
+{
+};
+
+#endif
