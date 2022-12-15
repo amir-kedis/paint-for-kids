b4d2276f1d27d3396e78a6f5b6633d0dcc7a964c
Akram Hany <akramhany65@gmail.com>
2022-12-15 20:23:09 +0200

    Finish CTriangle.h part

--- Figures/CTriangle.h
+++ Figures/CTriangle.h
@@ -3,8 +3,15 @@
 
 #include "CFigure.h"
 
-class CTriangle
+class CTriangle : public CFigure
 {
+private:
+	Point Corner1;
+	Point Corner2;
+	Point Corner3;
+public:
+	CTriangle(Point, Point, Point, GfxInfo FigureGfxInfo);
+	virtual void Draw(Output* pOut) const;
 };
 
 #endif
