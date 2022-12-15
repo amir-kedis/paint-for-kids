7d0b2e7ecc78cc9921c23bb723381af40fa10196
Akram Hany <akramhany65@gmail.com>
2022-12-15 20:37:04 +0200

    Finish writing data members and methods declaration in .h file

--- Actions/AddTriangleAction.h
+++ Actions/AddTriangleAction.h
@@ -5,6 +5,18 @@
 
 class AddTriangleAction : public Action
 {
+private:
+	Point P1, P2, P3;   //Triangle Corners
+	GfxInfo TriangleGfxInfo;
+public:
+	AddTriangleAction(ApplicationManager* pApp);
+
+	//Reads triangle parameters
+	virtual void ReadActionParameters();
+
+	//Add triangle to the ApplicationManager
+	virtual void Execute();
+
 };
 
 #endif
