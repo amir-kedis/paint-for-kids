ff1914365ca9411850d331585269b5940da52fcf
Akram Hany <akramhany65@gmail.com>
2022-12-16 16:24:27 +0200

    Create AddCircleAction class (.h and .cpp) files

---
+++ Actions/AddCircleAction.cpp
@@ -0,0 +1,7 @@
+#include "AddCircleAction.h"
+#include "..\Figures\CCircle.h"
+
+#include "..\ApplicationManager.h"
+
+#include "..\GUI\input.h"
+#include "..\GUI\Output.h"
---
+++ Actions/AddCircleAction.h
@@ -0,0 +1,10 @@
+#ifndef ADD_Circle_ACTION_H
+#define ADD_Circle_ACTION_H
+
+#include "Action.h"
+
+class AddCircleAction : public Action
+{
+};
+
+#endif
