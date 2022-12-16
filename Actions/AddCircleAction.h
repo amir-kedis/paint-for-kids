bcd1ba3db26122fd7e97f2c53db5ba766a4f2291
Akram Hany <akramhany65@gmail.com>
2022-12-16 16:27:51 +0200

    Finish writing data members and methods declaration in AddCircleAction.h

--- Actions/AddCircleAction.h
+++ Actions/AddCircleAction.h
@@ -5,6 +5,18 @@
 
 class AddCircleAction : public Action
 {
+private:
+	Point P1, P2;
+	GfxInfo RectGfxInfo;
+public:
+	AddCircleAction(ApplicationManager* pApp);
+
+	//Reads Circle parameters
+	virtual void ReadActionParameters();
+
+	//Add circle to the ApplicationManager
+	virtual void Execute();
+
 };
 
 #endif
