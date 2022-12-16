9fa2d79ed7ff8dbad1510aeedb2fc3212163207e
Akram Hany <akramhany65@gmail.com>
2022-12-16 16:39:35 +0200

    Create AddSquareAction class (.h and .cpp) files

---
+++ Actions/AddSquareAction.cpp
@@ -0,0 +1,7 @@
+#include "AddSquareAction.h"
+#include "..\Figures\CSquare.h"
+
+#include "..\ApplicationManager.h"
+
+#include "..\GUI\input.h"
+#include "..\GUI\Output.h"
---
+++ Actions/AddSquareAction.h
@@ -0,0 +1,10 @@
+#ifndef ADD_SEQUARE_ACTION_H
+#define ADD_SEQUARE_ACTION_H
+
+#include "Action.h"
+
+class AddSquareAction: public Action
+{
+};
+
+#endif
