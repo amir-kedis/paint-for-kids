c199024b1dd591ef7fe0a357d6cc2c8bbb35c76b
Akram Hany <akramhany65@gmail.com>
2022-12-16 16:05:15 +0200

    Finish Implementing methods in CSquare.cpp

--- Figures/CSquare.cpp
+++ Figures/CSquare.cpp
@@ -1,1 +1,13 @@
 #include "CSquare.h"
+
+CSquare::CSquare(Point P1, GfxInfo FigureGfxInfo):CFigure(FigureGfxInfo)
+{
+	Center = P1;
+}
+
+
+void CSquare::Draw(Output* pOut) const
+{
+	//Call Output::DrawSquare to draw a square on the screen	
+	pOut->DrawSquare(Center, FigGfxInfo, Selected);
+}
