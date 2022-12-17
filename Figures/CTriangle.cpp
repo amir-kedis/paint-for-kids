63be4a3ed7fdb2cbe3fd22dbece41f1b25b46377
ahmedhamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2022-12-17 02:26:56 +0200

    Create Save for Triangle

--- Figures/CTriangle.cpp
+++ Figures/CTriangle.cpp
@@ -12,3 +12,14 @@ void CTriangle::Draw(Output* pOut) const
 	//Call Output::DrawTriangle to draw a triangle on the screen	
 	pOut->DrawTriangle(Corner1, Corner2, Corner3, FigGfxInfo, Selected);
 }
+
+void CTriangle::Save(ofstream& OutFile, int ID) const
+{
+	OutFile << "TRIANG \t" << ID << '\t' << Corner1.x << '\t' << Corner1.y << '\t'
+		<< Corner2.x << '\t' << Corner2.y << '\t' << Corner3.x << '\t' << Corner3.y << '\t'
+		<< ColorToString(FigGfxInfo.DrawClr) << '\t';
+	if (FigGfxInfo.isFilled)
+		OutFile << ColorToString(FigGfxInfo.FillClr) << '\n';
+	else
+		OutFile << "NO_FILL\n";
+}
--- Figures/CTriangle.h
+++ Figures/CTriangle.h
@@ -12,6 +12,7 @@ private:
 public:
 	CTriangle(Point, Point, Point, GfxInfo FigureGfxInfo);
 	virtual void Draw(Output* pOut) const;
+	virtual void Save(ofstream& OutFile, int ID) const;
 };
 
 #endif
