9730eed0578eb8dd6f50c08a5f5bab0a4d2e824e
ahmedhamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2022-12-17 02:29:47 +0200

    Create Save for Circle

--- Figures/CCircle.cpp
+++ Figures/CCircle.cpp
@@ -12,3 +12,13 @@ void CCircle::Draw(Output* pOut) const
 	//Call Output::DrawCircle to draw a circle on the screen	
 	pOut->DrawCircle(Center, P, FigGfxInfo, Selected);
 }
+
+void CCircle::Save(ofstream& OutFile, int ID) const
+{
+	OutFile << "CIRCLE \t" << ID << '\t' << Center.x << '\t' << Center.y << '\t'
+		<< ColorToString(FigGfxInfo.DrawClr) << '\t';
+	if (FigGfxInfo.isFilled)
+		OutFile << ColorToString(FigGfxInfo.FillClr) << '\n';
+	else
+		OutFile << "NO_FILL\n";
+}
