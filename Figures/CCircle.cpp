e3f6e75e9951d2b5a97eeb1e3ddc8e2647e54d6e
amir-kedis <amir.kedis@gmail.com>
2022-12-19 05:42:33 +0200

    🪲 Fix Merge small Bug

--- Figures/CCircle.cpp
+++ Figures/CCircle.cpp
@@ -6,28 +6,29 @@ CCircle::CCircle(Point P1, Point P2, GfxInfo FigureGfxInfo) : CFigure(FigureGfxI
 	P = P2;
 }
 
-void CCircle::Draw(Output *pOut) const
+void CCircle::Draw(Output* pOut) const
 {
 	// Call Output::DrawCircle to draw a circle on the screen
 	pOut->DrawCircle(Center, P, FigGfxInfo, Selected);
 }
 
-void CCircle::Save(ofstream &OutFile, int ID) const
+void CCircle::Save(ofstream& OutFile, int ID) const
 {
 	OutFile << "CIRCLE \t" << ID << '\t' << Center.x << '\t' << Center.y << '\t'
-			<< ColorToString(FigGfxInfo.DrawClr) << '\t';
+		<< ColorToString(FigGfxInfo.DrawClr) << '\t';
 	if (FigGfxInfo.isFilled)
 		OutFile << ColorToString(FigGfxInfo.FillClr) << '\n';
 	else
 		OutFile << "NO_FILL\n";
-	bool CCircle::IsInFigure(Point CheckPoint) const
-	{
-		// radius is distance between center and point in parameter
-		double radiusOfCircle = sqrt(pow(Center.x - P.x, 2) + pow(Center.y - P.y, 2));
+}
+bool CCircle::IsInFigure(Point CheckPoint) const
+{
+	// radius is distance between center and point in parameter
+	double radiusOfCircle = sqrt(pow(Center.x - P.x, 2) + pow(Center.y - P.y, 2));
 
-		// distance between check point and center
-		double CheckRaduis = sqrt(pow(Center.x - CheckPoint.x, 2) + pow(Center.y - CheckPoint.y, 2));
+	// distance between check point and center
+	double CheckRaduis = sqrt(pow(Center.x - CheckPoint.x, 2) + pow(Center.y - CheckPoint.y, 2));
 
-		// Point is in circle if dist from it to center is less than the raduis
-		return CheckRaduis <= radiusOfCircle;
-	}
+	// Point is in circle if dist from it to center is less than the raduis
+	return CheckRaduis <= radiusOfCircle;
+}
