586ee0b9e2c31c0e22a0b7b2de4a5bf6f933365d
ahmedhamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2022-12-23 23:43:07 +0200

    Establish main parts of Color Action

--- Actions/ChangeColorAction.cpp
+++ Actions/ChangeColorAction.cpp
@@ -1,1 +1,18 @@
 #include "ChangeColorAction.h"
+
+ChangeColorAction::ChangeColorAction(ApplicationManager* pApp, char DOF, color C) : Action(pApp)
+{
+	DrawOrFill = DOF;
+	Color = C;
+}
+
+void ReadActionParameters()
+{
+
+}
+
+//Change Color of the figure
+void Execute()
+{
+
+}
--- Actions/ChangeColorAction.h
+++ Actions/ChangeColorAction.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "Action.h"
-class ChangeColorAction :
-    public Action
+class ChangeColorAction : public Action
 {
+private:
+	char DrawOrFill;
+	color Color;
+public:
+	ChangeColorAction(ApplicationManager* pApp, char DOF, color C);
+
+	virtual void ReadActionParameters();
+
+	//Change Color of the figure
+	virtual void Execute();
 };
 
